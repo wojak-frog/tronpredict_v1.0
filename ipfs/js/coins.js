04cf2d204ab353e3aa2192f33f8c2ea8c368dce8
wojak-frog <37157916+wojak-frog@users.noreply.github.com>
2020-10-10 15:27:58 +0300

    Update coins.js

--- ipfs/js/coins.js
+++ ipfs/js/coins.js
@@ -1,3 +1,3 @@
 //var coins = [[11,"BTC"],[12,"ETH"],[13,"XRP"],[14,"BCH"],[15,"LTC"],[16,"BNB"],[17,"EOS"],[18,"XTZ"],[19,"XLM"],[20,"ADA"],[21,"LINK"],[22,"XMR"],[23,"TRX"],[24,"NEO"],[25,"DASH"],[26,"ATOM"]];
 
-var coins = [[11,"BTC"],[12,"ETH"],[15,"LTC"],[17,"EOS"],[13,"XRP"],[14,"BCH"],[21,"LINK"],[23,"TRX"],[26,"ATOM"],[24,"NEO"]];
+var coins = [[11,"BTC"],[12,"TRX"],[13,"ETH"], [14,"SUN"], [15,"JST"], [16,"LTC"]];
